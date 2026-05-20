5305771004f1eb203fd26d5b520dc9c173dd18bb
Ferrolune <45581966+Ferrolune@users.noreply.github.com>
2026-05-20 18:27:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ buttons.forEach((button) => {
     button.style.transform = 'translateY(0px)';
   });
 });
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('mouseenter', () => {
+    button.style.transform = 'translateY(-2px)';
+  });
+
+  button.addEventListener('mouseleave', () => {
+    button.style.transform = 'translateY(0px)';
+  });
+});
+
+const joinButton = document.getElementById('join-community');
+
+joinButton.addEventListener('click', () => {
+  window.open('https://discord.gg/2NJvqgNKhV', '_blank');
+});
